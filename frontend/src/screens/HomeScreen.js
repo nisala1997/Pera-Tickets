f1da092e6d90b2a7521ef88ecfc4df7b4439294b
king-dilz <124921854+king-dilz@users.noreply.github.com>
2024-12-30 01:45:43 +0530

    DL: Re-configured the home screen.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,39 +7,71 @@ import Message from '../components/Message'
 import Paginate from '../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
 import { listProducts } from '../actions/productActions'
+import {products} from '../products'
+
+
+// function HomeScreen({ history }) {
+//     const dispatch = useDispatch()
+//     // const productList = useSelector(state => state.productList)
+//     // const { error, loading, products, page, pages } = productList
+
+//     let keyword = history.location.search
+
+//     useEffect(() => {
+//         dispatch(listProducts(keyword))
+
+//     }, [dispatch, keyword])
+
+//     return (
+//         <div>
+//             {!keyword && <ProductCarousel />}
+
+//             <h1>Upcoming Events</h1>
+            
+//                     <div>
+//                         <Row>
+//                             {products.map(product => (
+//                                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+//                                     <Product product={product} />
+//                                 </Col>
+//                             ))}
+//                         </Row>
+//                         <Paginate page={page} pages={pages} keyword={keyword} />
+//                     </div>
+
+//         </div>
+//     )
+// }
+
+// export default HomeScreen
+
 
 
 function HomeScreen({ history }) {
-    const dispatch = useDispatch()
-    const productList = useSelector(state => state.productList)
-    const { error, loading, products, page, pages } = productList
-
-    let keyword = history.location.search
-
-    useEffect(() => {
-        dispatch(listProducts(keyword))
-
-    }, [dispatch, keyword])
+   
 
     return (
         <div>
-            {!keyword && <ProductCarousel />}
+            {/* Show carousel only if there's no search keyword */}
+            {!history.location.search && <ProductCarousel />}
 
-            <h1>Latest Products</h1>
-            {loading ? <Loader />
-                : error ? <Message variant='danger'>{error}</Message>
-                    :
-                    <div>
-                        <Row>
-                            {products.map(product => (
-                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                    <Product product={product} />
-                                </Col>
-                            ))}
-                        </Row>
-                        <Paginate page={page} pages={pages} keyword={keyword} />
-                    </div>
-            }
+            <h1>Upcoming Events</h1>
+
+            <div>
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+                {/* Pass pages and page to Paginate */}
+                {/* <Paginate
+                    pages={pages}
+                    page={page}
+                    keyword={history.location.search}
+                /> */}
+            </div>
         </div>
     )
 }
