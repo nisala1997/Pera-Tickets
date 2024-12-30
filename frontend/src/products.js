f1ac13df64e65916f362e4ff1476c452fb6572eb
nisala1997 <122119249+nisala1997@users.noreply.github.com>
2024-12-30 21:39:52 +0530

    Add

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,7 +1,7 @@
 export const products = [
   {
     '_id': '1',
-    'name': 'Airpods Wireless Bluetooth Headphones',
+    'name': 'Geethanjalee',
     'image': '/images/airpods.jpg',
     'description':
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
@@ -14,7 +14,7 @@ export const products = [
   },
   {
     '_id': '2',
-    'name': 'iPhone 11 Pro 256GB Memory',
+    'name': 'Pahan Tharuwa',
     'image': '/images/phone.jpg',
     'description':
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
@@ -27,7 +27,7 @@ export const products = [
   },
   {
     '_id': '3',
-    'name': 'Cannon EOS 80D DSLR Camera',
+    'name': 'Nethrawanee',
     'image': '/images/camera.jpg',
     'description':
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
@@ -40,7 +40,7 @@ export const products = [
   },
   {
     '_id': '4',
-    'name': 'Sony Playstation 4 Pro White Version',
+    'name': 'Geethanjalee',
     'image': '/images/playstation.jpg',
     'description':
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -53,7 +53,7 @@ export const products = [
   },
   {
     '_id': '5',
-    'name': 'Logitech G-Series Gaming Mouse',
+    'name': 'Indeewari',
     'image': '/images/mouse.jpg',
     'description':
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
@@ -66,7 +66,7 @@ export const products = [
   },
   {
     '_id': '6',
-    'name': 'Amazon Echo Dot 3rd Generation',
+    'name': 'Avarjana',
     'image': '/images/alexa.png',
     'description':
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
