b955f9443291a81248b9b1defc8caff484882cca
nisala1997 <122119249+nisala1997@users.noreply.github.com>
2024-12-30 20:59:49 +0530

    update

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -67,7 +67,7 @@ export const products = [
   {
     '_id': '6',
     'name': 'Amazon Echo Dot 3rd Generation',
-    'image': '/images/alexa.jpg',
+    'image': '/images/alexa.png',
     'description':
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     'brand': 'Amazon',
